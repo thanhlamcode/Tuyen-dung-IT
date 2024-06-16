83063b4920f6aa048b7d0bbf2bfe40000bba3a70
thanhlamcode <lamdoan1122334455@gmail.com>
2024-06-16 17:27:31 +0700

    q

--- it-recruiment/src/pages/Login/index.js
+++ it-recruiment/src/pages/Login/index.js
@@ -25,12 +25,12 @@ function Login() {
           autoComplete="off"
         >
           <Form.Item
-            label="Username"
-            name="username"
+            label="Email"
+            name="email"
             rules={[
               {
                 required: true,
-                message: "Please input your username!",
+                type: "email",
               },
             ]}
           >
